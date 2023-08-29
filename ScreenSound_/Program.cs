22c774833749297c4cce366a23b0e9ac9781bd30
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-08-29 09:42:59 -0300

    criando opções

--- ScreenSound_/Program.cs
+++ ScreenSound_/Program.cs
@@ -24,7 +24,44 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a sua opção: ");
-    Console.ReadLine();
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        case 2: 
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        case 5:
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            //Console.WriteLine("Você escolheu a opcão " + opcaoEscolhidaNumerica);
+            break;
+        default: Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("Digite o nome da banda que deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeBanda} foi registrada!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
 }
 
 ExibirOpcoesDoMenu();
+RegistrarBanda();
